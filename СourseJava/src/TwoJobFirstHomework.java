ada43fc2b9bf531b2701f2b31a52b19353484592
risen777 <21risen@gmail.com>
2017-03-13 23:41:08 +0300

    Второе дз

---
+++ СourseJava/src/TwoJobFirstHomework.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Created by Gunnik on 13.03.2017.
+ */
+public class TwoJobFirstHomework {
+    public static void main(String[] args) {
+        System.out.println("Введи число");
+        Scanner scanner =new Scanner(System.in);
+        int value = scanner.nextInt();
+        while (value<=5 || value>=155) {
+            System.out.println("Введи число (25;100)");
+            value = scanner.nextInt();
+        }
+if (value>25 && value<100) {
+    System.out.println("Число" +" "+ value +" "+ "содержится в интервале (25,100)");
+}
+else System.out.println("Число" +" "+ value +" " + "не содержится в интервале (25,100)");
+
+        }
+
+    }
+
+
---
+++ СourseJava/src/TwoJobSecondHomework.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Gunnik on 13.03.2017.
+ */
+public class TwoJobSecondHomework {
+    public static void main(String[] args) {
+        int min = 100;
+        int max =999;
+        int value = (int) (Math.random()*(max-min));
+
+      int  a = value/100;
+      int  b = (value%100)/10;
+      int  c = value%10;
+        if(a>=b&&a>=c) {
+            System.out.println("В числе "+value+" наибольшая цифра "+a);
+        } else if(b>=a&&b>=c) {
+            System.out.println("В числе "+value+" наибольшая цифра "+b);
+        } else {
+            System.out.println("В числе "+value+" наибольшая цифра "+c);
+        }
+
+    }
+
+
+
+
+}
