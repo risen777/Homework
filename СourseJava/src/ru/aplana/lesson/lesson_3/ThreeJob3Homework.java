f9d6d15bd0a69a09c45e35a49509ff07656461db
risen777 <21risen@gmail.com>
2017-03-22 13:03:12 +0300

    3 dz

---
+++ СourseJava/src/ru/aplana/lesson/lesson_3/ThreeJob3Homework.java
@@ -0,0 +1,20 @@
+package ru.aplana.lesson.lesson_3;
+
+import java.util.Arrays;
+
+/**
+ * Created by user on 22.03.2017.
+ */
+public class ThreeJob3Homework {
+    public static void main(String[] args) {
+
+                int mas[] = {1, 2, 3};
+                int i;
+        for (int v : mas)
+                System.out.println(v);
+
+                for(i = mas.length - 1; i >= 0; i--) {
+                    System.out.print(mas[i]+" ");
+                }
+            }
+        }
