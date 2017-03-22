862e4ad9cd3ce19bd449629594221be583c0218e
risen777 <21risen@gmail.com>
2017-03-22 14:51:10 +0300

    3 4 dz

---
+++ СourseJava/src/ru/aplana/lesson/lesson_3/ThreeJob4Homework.java
@@ -0,0 +1,27 @@
+package ru.aplana.lesson.lesson_3;
+
+import java.util.Random;
+
+
+public class ThreeJob4Homework {
+
+    //Лучше нет времени написать ибо работа
+    public static void main(String[] args){
+        char arrGai[] = {  // массив
+                'Б', 'В', 'Г', 'Д', 'Ж', 'З', 'К', 'Л', 'М', 'Н', 'П', 'Р', 'С', 'Т', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ' };
+        //я хз какие там катят буквы можно дополнить
+
+
+        Random rand = new Random();
+        int n =rand.nextInt(999);
+//Лучше нет времени написать ибо работа
+            int r = rand.nextInt(9);
+            int z = rand.nextInt(9);
+            int y = rand.nextInt(95);
+            int f = rand.nextInt(9);
+            System.out.print(arrGai[f]+" "+n +" " + arrGai[r]+ arrGai[z]+" "+y );
+        } //
+
+    }  //
+//
+//Лучше нет времени написать ибо работа
